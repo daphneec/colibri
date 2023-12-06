2d145f403507711edbc2dd36b6a1a407e1097e61
Daphnee Chabal <daphneechabal@fralwb-self-145-109-118-186.wireless.uva.nl>
2023-12-06 13:40:59 +0100

    changed model_profiling as well as the secure version to calculate seconds, not calculate params nor nmacs anymore, and hardcoded seconds values, in waiting for the analytical model.

--- utils/model_profiling.py
+++ utils/model_profiling.py
@@ -52,12 +52,12 @@ def get_params(self):
 
 
 def run_forward(self, input, num_forwards=10):
-    if num_forwards <= 0:
-        return 0.0
-    with Timer() as t:
-        for _ in range(num_forwards):
-            self.forward(*input)
-            torch.cuda.synchronize()
+    #if num_forwards <= 0:
+    #    return 0.0
+    #with Timer() as t:
+    #    for _ in range(num_forwards):
+    #        self.forward(*input)
+    #        torch.cuda.synchronize()
     return int(t.time * 1e9 / num_forwards)
 
 
@@ -77,11 +77,11 @@ def conv_module_name_filter(name):
 
 def module_profiling(self, input, output, num_forwards, verbose):
     def add_sub(m, sub_op):
-        m.n_macs += getattr(sub_op, 'n_macs', 0)
-        m.n_params += getattr(sub_op, 'n_params', 0)
+        #m.n_macs += getattr(sub_op, 'n_macs', 0)
+        #m.n_params += getattr(sub_op, 'n_params', 0)
         m.n_seconds += getattr(sub_op, 'n_seconds', 0)
-
-    _run_forward = functools.partial(run_forward, num_forwards=num_forwards)
+        
+    #_run_forward = functools.partial(run_forward, num_forwards=num_forwards)
     # if isinstance(self, (hr.ParallelModule, hr.FuseModule, hr.HeadModule)) \
     #     or (isinstance(self, nn.Sequential) and isinstance(self[0], hr.ParallelModule)):
     if not input:
@@ -96,47 +96,53 @@ def module_profiling(self, input, output, num_forwards, verbose):
         t = type(self)
         self._profiling_input_size = ins
         self._profiling_output_size = outs
-    if isinstance(self, cnn.Conv2d):
-        self.n_macs = (ins[1] * outs[1] * self.kernel_size[0] *
-                       self.kernel_size[1] * outs[2] * outs[3] //
-                       self.groups) * outs[0]
-        self.n_params = get_params(self)
-        self.n_seconds = _run_forward(self, input)
+    if isinstance(self, nn.Conv2d):
+        #self.n_macs = (ins[1] * outs[1] * self.kernel_size[0] *
+        #               self.kernel_size[1] * outs[2] * outs[3] //
+        #               self.groups) * outs[0]
+        #self.n_params = get_params(self)
+        #self.n_seconds = _run_forward(self, input)
         self.name = conv_module_name_filter(self.__repr__())
+        self.n_seconds = 0.001
     elif isinstance(self, nn.ConvTranspose2d):
-        self.n_macs = (ins[1] * outs[1] * self.kernel_size[0] *
-                       self.kernel_size[1] * outs[2] * outs[3] //
-                       self.groups) * outs[0]
-        self.n_params = get_params(self)
-        self.n_seconds = _run_forward(self, input)
+        #self.n_macs = (ins[1] * outs[1] * self.kernel_size[0] *
+        #               self.kernel_size[1] * outs[2] * outs[3] //
+        #               self.groups) * outs[0]
+        #self.n_params = get_params(self)
+        #self.n_seconds = _run_forward(self, input)
         self.name = conv_module_name_filter(self.__repr__())
-    elif isinstance(self, cnn.Linear):
-        self.n_macs = ins[1] * outs[1] * outs[0]
-        self.n_params = get_params(self)
-        self.n_seconds = _run_forward(self, input)
+        self.n_seconds = 0.001
+    elif isinstance(self, nn.Linear):
+        #self.n_macs = ins[1] * outs[1] * outs[0]
+        #self.n_params = get_params(self)
+        #self.n_seconds = _run_forward(self, input)
         self.name = self.__repr__()
+        self.n_seconds = 0.001
     elif isinstance(self, cnn.AvgPool2d):
         # NOTE: this function is correct only when stride == kernel size
-        self.n_macs = ins[1] * ins[2] * ins[3] * ins[0]
-        self.n_params = 0
-        self.n_seconds = _run_forward(self, input)
+        #self.n_macs = ins[1] * ins[2] * ins[3] * ins[0]
+        #self.n_params = 0
+        #self.n_seconds = _run_forward(self, input)
         self.name = self.__repr__()
+        self.n_seconds = 0.001
     elif isinstance(self, cnn.AdaptiveAvgPool2d):
         # NOTE: this function is correct only when stride == kernel size
-        self.n_macs = ins[1] * ins[2] * ins[3] * ins[0]
-        self.n_params = 0
-        self.n_seconds = _run_forward(self, input)
+        #self.n_macs = ins[1] * ins[2] * ins[3] * ins[0]
+        #self.n_params = 0
+        #self.n_seconds = _run_forward(self, input)
         self.name = self.__repr__()
+        self.n_seconds = 0.001
     elif isinstance(self, smb.SqueezeAndExcitation):
-        self.n_macs = ins[1] * ins[2] * ins[3] * ins[0]
-        self.n_params = 0
+        #self.n_macs = ins[1] * ins[2] * ins[3] * ins[0]
+        #self.n_params = 0
         self.n_seconds = 0
         add_sub(self, self.se_reduce)
         add_sub(self, self.se_expand)
         self.name = self.__repr__()
+    
     elif isinstance(self, smb.InvertedResidualChannels):
-        self.n_macs = 0
-        self.n_params = 0
+        #self.n_macs = 0
+        #self.n_params = 0
         self.n_seconds = 0
         for op in self.ops:
             add_sub(self, op)
@@ -149,8 +155,8 @@ def module_profiling(self, input, output, num_forwards, verbose):
             add_sub(self, self.transformer)
         self.name = self.__repr__()
     elif isinstance(self, smb.InvertedResidualChannelsFused):
-        self.n_macs = 0
-        self.n_params = 0
+        #self.n_macs = 0
+        #self.n_params = 0
         self.n_seconds = 0
         for op in self.depth_ops:
             add_sub(self, op)
@@ -159,23 +165,23 @@ def module_profiling(self, input, output, num_forwards, verbose):
         add_sub(self, self.se_op)
         self.name = self.__repr__()
     elif isinstance(self, shr.ParallelModule):
-        self.n_macs = 0
-        self.n_params = 0
+        #self.n_macs = 0
+        #self.n_params = 0
         self.n_seconds = 0
         for op in self.branches:
             add_sub(self, op)
         self.name = self.__repr__()
     elif isinstance(self, shr.FuseModule):
-        self.n_macs = 0
-        self.n_params = 0
+        #self.n_macs = 0
+        #self.n_params = 0
         self.n_seconds = 0
         for ops in self.fuse_layers:
             for op in ops:
                 add_sub(self, op)
         self.name = self.__repr__()
     elif isinstance(self, shr.HeadModule):
-        self.n_macs = 0
-        self.n_params = 0
+        #self.n_macs = 0
+        #self.n_params = 0
         self.n_seconds = 0
         for op in self.incre_modules:
             add_sub(self, op)
@@ -184,8 +190,8 @@ def module_profiling(self, input, output, num_forwards, verbose):
         add_sub(self, self.final_layer)
         self.name = self.__repr__()
     elif isinstance(self, secure_transformer.secure_Transformer):
-        self.n_macs = 0
-        self.n_params = 0
+        #self.n_macs = 0
+        #self.n_params = 0
         self.n_seconds = 0
         add_sub(self, self.input_proj)
         add_sub(self, self.reverse_proj)
@@ -193,32 +199,32 @@ def module_profiling(self, input, output, num_forwards, verbose):
         add_sub(self, self.decoder)
         self.name = self.__repr__()
     elif isinstance(self, secure_transformer.secure_TransformerEncoderLayer):
-        self.n_macs = 0
-        self.n_params = 0
+        #self.n_macs = 0
+        #self.n_params = 0
         self.n_seconds = 0
         add_sub(self, self.self_attn)
         add_sub(self, self.linear1)
         add_sub(self, self.linear2)
         self.name = self.__repr__()
     elif isinstance(self, secure_transformer.secure_TransformerDecoderLayer):
-        self.n_macs = 0
-        self.n_params = 0
+        #self.n_macs = 0
+        #self.n_params = 0
         self.n_seconds = 0
         add_sub(self, self.multihead_attn)
         add_sub(self, self.linear1)
         add_sub(self, self.linear2)
         self.name = self.__repr__()
     elif isinstance(self, nn.MultiheadAttention):
-        self.n_macs = 0
-        self.n_params = 0
+        #self.n_macs = 0
+        #self.n_params = 0
         self.n_seconds = 0
         add_sub(self, self.out_proj)
-        self.n_macs += 2 * input[0].shape[0] * input[1].shape[0] * input[0].shape[2] + \
-            4 * input[0].shape[0] * input[0].shape[2] * input[0].shape[2]
+        #self.n_macs += 2 * input[0].shape[0] * input[1].shape[0] * input[0].shape[2] + \
+        #    4 * input[0].shape[0] * input[0].shape[2] * input[0].shape[2]
         self.name = self.__repr__()
     elif isinstance(self, shrb.HighResolutionModule):
-        self.n_macs = 0
-        self.n_params = 0
+        #self.n_macs = 0
+        #self.n_params = 0
         self.n_seconds = 0
         for op in self.branches:
             add_sub(self, op)
@@ -226,16 +232,16 @@ def module_profiling(self, input, output, num_forwards, verbose):
             add_sub(self, op)
         self.name = self.__repr__()
     elif isinstance(self, shrb.BasicBlock):
-        self.n_macs = 0
-        self.n_params = 0
+        #self.n_macs = 0
+        #self.n_params = 0
         self.n_seconds = 0
         add_sub(self, self.conv1)
         if self.downsample is not None:
             add_sub(self, self.downsample)
         self.name = self.__repr__()
     elif isinstance(self, shrb.Bottleneck):
-        self.n_macs = 0
-        self.n_params = 0
+        #self.n_macs = 0
+        #self.n_params = 0
         self.n_seconds = 0
         add_sub(self, self.conv1)
         add_sub(self, self.conv2)
@@ -245,14 +251,14 @@ def module_profiling(self, input, output, num_forwards, verbose):
         self.name = self.__repr__()
     else:
         # This works only in depth-first travel of modules.
-        self.n_macs = 0
-        self.n_params = 0
+        #self.n_macs = 0
+        #self.n_params = 0
         self.n_seconds = 0
         num_children = 0
         for m in self.children():
-            self.n_macs += getattr(m, 'n_macs', 0)
-            self.n_params += getattr(m, 'n_params', 0)
-            self.n_seconds += getattr(m, 'n_seconds', 0)
+            #self.n_macs += getattr(m, 'n_macs', 0)
+            #self.n_params += getattr(m, 'n_params', 0)
+            self.n_seconds += 0.001#getattr(m, 'n_seconds', 0)
             num_children += 1
         ignore_zeros_t = [
             cnn.BatchNorm2d,
@@ -269,18 +275,18 @@ def module_profiling(self, input, output, num_forwards, verbose):
             nn.modules.padding.ZeroPad2d,
             cnn.Sigmoid,
         ]
-        if (not getattr(self, 'ignore_model_profiling', False) and
-                self.n_macs == 0 and t not in ignore_zeros_t):
-            if udist.is_master():
-                logging.info('WARNING: leaf module {} has zero n_macs.'.format(
-                    type(self)))
+        #if (not getattr(self, 'ignore_model_profiling', False) and
+        #        self.n_macs == 0 and t not in ignore_zeros_t):
+        #    if udist.is_master():
+        #        logging.info('WARNING: leaf module {} has zero n_macs.'.format(
+        #            type(self)))
         return
     if verbose:
         if udist.is_master():
             logging.info(
                 self.name.ljust(name_space, ' ') +
-                '{:,}'.format(self.n_params).rjust(params_space, ' ') +
-                '{:,}'.format(self.n_macs).rjust(macs_space, ' ') +
+                #'{:,}'.format(self.n_params).rjust(params_space, ' ') +
+                #'{:,}'.format(self.n_macs).rjust(macs_space, ' ') +
                 '{:,}'.format(self.n_seconds).rjust(seconds_space, ' '))
     return
 
@@ -333,8 +339,8 @@ def model_profiling(model,
     model.apply(lambda m: add_profiling_hooks(m, num_forwards, verbose=verbose))
     if verbose:
         logging.info('Item'.ljust(name_space, ' ') +
-                     'params'.rjust(macs_space, ' ') +
-                     'macs'.rjust(macs_space, ' ') +
+                    # 'params'.rjust(macs_space, ' ') +
+                    # 'macs'.rjust(macs_space, ' ') +
                      'nanosecs'.rjust(seconds_space, ' '))
         logging.info(''.center(
             name_space + params_space + macs_space + seconds_space, '-'))
@@ -344,9 +350,9 @@ def model_profiling(model,
         logging.info(''.center(
             name_space + params_space + macs_space + seconds_space, '-'))
         logging.info('Total'.ljust(name_space, ' ') +
-                     '{:,}'.format(model.n_params).rjust(params_space, ' ') +
-                     '{:,}'.format(model.n_macs).rjust(macs_space, ' ') +
+                    # '{:,}'.format(model.n_params).rjust(params_space, ' ') +
+                    # '{:,}'.format(model.n_macs).rjust(macs_space, ' ') +
                      '{:,}'.format(model.n_seconds).rjust(seconds_space, ' '))
     remove_profiling_hooks()
     model = model.to(origin_device)
-    return model.n_macs, model.n_params
+    return model.n_seconds#model.n_macs, model.n_params
